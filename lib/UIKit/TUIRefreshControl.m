4ce0e9efa1041947e0d62499cbb0fb6f428a199b
Aditya Nrusimha <aditya.nrusimha@gmail.com>
2012-09-24 15:53:48 -0700

    Fixed massive breakages.

--- lib/UIKit/TUIRefreshControl.m
+++ lib/UIKit/TUIRefreshControl.m
@@ -18,7 +18,7 @@ static CGFloat const TUIRefreshMaxArrowRadius = 7;
 static CGFloat const TUIRefreshMaxDistance = 53;
 
 static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p) {
-    return a + (b - a) * p;
+	return a + (b - a) * p;
 }
 
 @interface TUIRefreshControl ()
@@ -36,75 +36,75 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p) {
 @implementation TUIRefreshControl
 
 - (id)initInTableView:(TUITableView *)tableView {
-    if((self = [super initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, TUIRefreshTotalHeight)])) {
-        self.autoresizingMask = TUIViewAutoresizingFlexibleWidth;
-        
+	if((self = [super initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, TUIRefreshTotalHeight)])) {
+		self.autoresizingMask = TUIViewAutoresizingFlexibleWidth;
+		
 		self.tableView = tableView;
-        [self.tableView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
-        [self.tableView setPullDownView:self];
-        
-        self.activity = [[TUIActivityIndicatorView alloc] initWithActivityIndicatorStyle:TUIActivityIndicatorViewStyleGray];
-        self.activity.autoresizingMask = TUIViewAutoresizingFlexibleLeftMargin | TUIViewAutoresizingFlexibleRightMargin;
-        self.activity.alpha = 0.0f;
-        [self addSubview:self.activity];
-        
-        self.refreshing = NO;
-        self.tintColor = [NSColor colorWithCalibratedRed:155.0 / 255.0 green:162.0 / 255.0 blue:172.0 / 255.0 alpha:1.0];
-        
-        self.shapeLayer = [CAShapeLayer layer];
-        self.shapeLayer.fillColor = [_tintColor CGColor];
-        self.shapeLayer.strokeColor = [[[NSColor darkGrayColor] colorWithAlphaComponent:0.5] CGColor];
-        self.shapeLayer.lineWidth = 0.5;
-        self.shapeLayer.shadowColor = [[NSColor blackColor] CGColor];
-        self.shapeLayer.shadowOffset = CGSizeMake(0, 1);
-        self.shapeLayer.shadowOpacity = 0.4;
-        self.shapeLayer.shadowRadius = 0.5;
-        [self.layer addSublayer:self.shapeLayer];
-        
-        self.arrowLayer = [CAShapeLayer layer];
-        self.arrowLayer.strokeColor = [[[NSColor darkGrayColor] colorWithAlphaComponent:0.5] CGColor];
-        self.arrowLayer.lineWidth = 0.5;
-        self.arrowLayer.fillColor = [[NSColor whiteColor] CGColor];
-        [self.shapeLayer addSublayer:self.arrowLayer];
-    }
+		[self.tableView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
+		[self.tableView setPullDownView:self];
+		
+		self.activity = [[TUIActivityIndicatorView alloc] initWithActivityIndicatorStyle:TUIActivityIndicatorViewStyleGray];
+		self.activity.autoresizingMask = TUIViewAutoresizingFlexibleLeftMargin | TUIViewAutoresizingFlexibleRightMargin;
+		self.activity.alpha = 0.0f;
+		[self addSubview:self.activity];
+		
+		self.refreshing = NO;
+		self.tintColor = [NSColor colorWithCalibratedRed:155.0 / 255.0 green:162.0 / 255.0 blue:172.0 / 255.0 alpha:1.0];
+		
+		self.shapeLayer = [CAShapeLayer layer];
+		self.shapeLayer.fillColor = [_tintColor CGColor];
+		self.shapeLayer.strokeColor = [[[NSColor darkGrayColor] colorWithAlphaComponent:0.5] CGColor];
+		self.shapeLayer.lineWidth = 0.5;
+		self.shapeLayer.shadowColor = [[NSColor blackColor] CGColor];
+		self.shapeLayer.shadowOffset = CGSizeMake(0, 1);
+		self.shapeLayer.shadowOpacity = 0.4;
+		self.shapeLayer.shadowRadius = 0.5;
+		[self.layer addSublayer:self.shapeLayer];
+		
+		self.arrowLayer = [CAShapeLayer layer];
+		self.arrowLayer.strokeColor = [[[NSColor darkGrayColor] colorWithAlphaComponent:0.5] CGColor];
+		self.arrowLayer.lineWidth = 0.5;
+		self.arrowLayer.fillColor = [[NSColor whiteColor] CGColor];
+		[self.shapeLayer addSublayer:self.arrowLayer];
+	}
 	
-    return self;
+	return self;
 }
 
 - (void)dealloc {
-    [self.tableView removeObserver:self forKeyPath:@"contentOffset"];
-    self.tableView = nil;
+	[self.tableView removeObserver:self forKeyPath:@"contentOffset"];
+	self.tableView = nil;
 }
 
 - (void)willMoveToSuperview:(TUIView *)newSuperview {
-    [super willMoveToSuperview:newSuperview];
+	[super willMoveToSuperview:newSuperview];
 	
-    if(!newSuperview) {
-        [self.tableView removeObserver:self forKeyPath:@"contentOffset"];
-        self.tableView = nil;
-    }
+	if(!newSuperview) {
+		[self.tableView removeObserver:self forKeyPath:@"contentOffset"];
+		self.tableView = nil;
+	}
 }
 
 - (void)setEnabled:(BOOL)enabled {
-    super.enabled = enabled;
-    self.shapeLayer.hidden = !self.enabled;
+	super.enabled = enabled;
+	self.shapeLayer.hidden = !self.enabled;
 }
 
 - (void)setTintColor:(NSColor *)tintColor {
-    _tintColor = tintColor;
-    self.shapeLayer.fillColor = _tintColor.CGColor;
+	_tintColor = tintColor;
+	self.shapeLayer.fillColor = _tintColor.CGColor;
 }
 
 - (void)beginRefreshing {
-    if(!self.refreshing) {
+	if(!self.refreshing) {
 		CABasicAnimation *alphaAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
-        alphaAnimation.duration = 0.1f;
-        alphaAnimation.toValue = [NSNumber numberWithFloat:0.0f];
-        alphaAnimation.fillMode = kCAFillModeForwards;
-        alphaAnimation.removedOnCompletion = NO;
-        [self.shapeLayer addAnimation:alphaAnimation forKey:nil];
-        [self.arrowLayer addAnimation:alphaAnimation forKey:nil];
-        
+		alphaAnimation.duration = 0.1f;
+		alphaAnimation.toValue = [NSNumber numberWithFloat:0.0f];
+		alphaAnimation.fillMode = kCAFillModeForwards;
+		alphaAnimation.removedOnCompletion = NO;
+		[self.shapeLayer addAnimation:alphaAnimation forKey:nil];
+		[self.arrowLayer addAnimation:alphaAnimation forKey:nil];
+		
 		[TUIView setAnimationsEnabled:NO block:^{
 			self.activity.frame = CGRectMake(self.bounds.size.width / 2 - self.activity.bounds.size.width / 2,
 											 TUIRefreshMaxDistance / 2 - self.activity.bounds.size.height / 2,
@@ -122,88 +122,88 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p) {
 }
 
 - (void)endRefreshing {
-    if(self.refreshing) {
-        self.refreshing = NO;
+	if(self.refreshing) {
+		self.refreshing = NO;
 		
-        [TUIView animateWithDuration:0.4 animations:^{
-            self.activity.alpha = 0.0f;
+		[TUIView animateWithDuration:0.4 animations:^{
+			self.activity.alpha = 0.0f;
 			[self.activity stopAnimating];
 			
 			[self.tableView scrollToRowAtIndexPath:self.tableView.indexPathForFirstRow
 								  atScrollPosition:TUITableViewScrollPositionTop
 										  animated:YES];
-        } completion:^(BOOL finished) {
-            [self.shapeLayer removeAllAnimations];
-            [self.arrowLayer removeAllAnimations];
+		} completion:^(BOOL finished) {
+			[self.shapeLayer removeAllAnimations];
+			[self.arrowLayer removeAllAnimations];
 			
 			TUIEdgeInsets preInset = self.tableView.contentInset;
 			preInset.top = self.bounds.origin.y;
 			self.tableView.contentInset = preInset;
 			
-            self.shapeLayer.position = CGPointZero;
+			self.shapeLayer.position = CGPointZero;
 			
-            self.shapeLayer.path = nil;
-            self.shapeLayer.shadowPath = nil;
-            self.arrowLayer.path = nil;
-        }];
-    }
+			self.shapeLayer.path = nil;
+			self.shapeLayer.shadowPath = nil;
+			self.arrowLayer.path = nil;
+		}];
+	}
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
 	if(![keyPath isEqualToString:@"contentOffset"] || self.refreshing)
 		return;
-    BOOL refreshTriggered = NO;
+	BOOL refreshTriggered = NO;
 	
 	CGFloat offset = self.tableView.pullOffset.y + self.tableView.bounceOffset.y;
 	CGFloat inset = self.bounds.origin.y - offset;
 	
 	CGFloat verticalShift = MAX(0, -((TUIRefreshMaxTopRadius + TUIRefreshMaxBottomRadius + TUIRefreshMaxTopPadding + TUIRefreshMaxBottomPadding) + offset));
-    CGFloat distance = MIN(TUIRefreshMaxDistance, fabs(verticalShift));
-    CGFloat percentage = 1 - (distance / TUIRefreshMaxDistance);
+	CGFloat distance = MIN(TUIRefreshMaxDistance, fabs(verticalShift));
+	CGFloat percentage = 1 - (distance / TUIRefreshMaxDistance);
 	CGFloat radius = lerp(TUIRefreshMinBottomRadius, TUIRefreshMaxBottomRadius, 0.2);
 	
-    CGFloat currentTopPadding = lerp(TUIRefreshMinTopPadding, TUIRefreshMaxTopPadding, percentage);
-    CGFloat currentTopRadius = lerp(TUIRefreshMinTopRadius, TUIRefreshMaxTopRadius, percentage);
-    CGFloat currentBottomRadius = lerp(TUIRefreshMinBottomRadius, TUIRefreshMaxBottomRadius, percentage);
-    CGFloat currentBottomPadding =  lerp(TUIRefreshMinBottomPadding, TUIRefreshMaxBottomPadding, percentage);
+	CGFloat currentTopPadding = lerp(TUIRefreshMinTopPadding, TUIRefreshMaxTopPadding, percentage);
+	CGFloat currentTopRadius = lerp(TUIRefreshMinTopRadius, TUIRefreshMaxTopRadius, percentage);
+	CGFloat currentBottomRadius = lerp(TUIRefreshMinBottomRadius, TUIRefreshMaxBottomRadius, percentage);
+	CGFloat currentBottomPadding =  lerp(TUIRefreshMinBottomPadding, TUIRefreshMaxBottomPadding, percentage);
 	
 	CGFloat currentArrowSize = lerp(TUIRefreshMinArrowSize, TUIRefreshMaxArrowSize, percentage);
 	CGFloat currentArrowRadius = lerp(TUIRefreshMinArrowRadius, TUIRefreshMaxArrowRadius, percentage);
 	CGFloat arrowBigRadius = currentArrowRadius + (currentArrowSize / 2);
 	CGFloat arrowSmallRadius = currentArrowRadius - (currentArrowSize / 2);
 	
-    CGPoint topOrigin = CGPointMake(roundf(self.bounds.size.width / 2),
+	CGPoint topOrigin = CGPointMake(roundf(self.bounds.size.width / 2),
 									inset - currentTopPadding - currentTopRadius);
-    CGPoint bottomOrigin = topOrigin;
-    if(distance != 0) {
-        bottomOrigin = CGPointMake(roundf(self.bounds.size.width / 2),
+	CGPoint bottomOrigin = topOrigin;
+	if(distance != 0) {
+		bottomOrigin = CGPointMake(roundf(self.bounds.size.width / 2),
 								   inset + offset + currentBottomPadding + currentBottomRadius);
-        if(percentage == 0) {
-            topOrigin.y -= (fabs(verticalShift) - TUIRefreshMaxDistance);
-            refreshTriggered = YES;
-        }
-    }
+		if(percentage == 0) {
+			topOrigin.y -= (fabs(verticalShift) - TUIRefreshMaxDistance);
+			refreshTriggered = YES;
+		}
+	}
 	
 	CGMutablePathRef path = CGPathCreateMutable();
 	CGPathAddArc(path, NULL, topOrigin.x, topOrigin.y, currentTopRadius, 0, M_PI, NO);
-    CGPoint leftCp1 = CGPointMake(lerp((topOrigin.x - currentTopRadius),
+	CGPoint leftCp1 = CGPointMake(lerp((topOrigin.x - currentTopRadius),
 									   (bottomOrigin.x - currentBottomRadius), 0.1),
 								  lerp(topOrigin.y, bottomOrigin.y, 0.2));
-    CGPoint leftCp2 = CGPointMake(lerp((topOrigin.x - currentTopRadius),
+	CGPoint leftCp2 = CGPointMake(lerp((topOrigin.x - currentTopRadius),
 									   (bottomOrigin.x - currentBottomRadius), 0.9),
 								  lerp(topOrigin.y, bottomOrigin.y, 0.2));
-    CGPoint leftDestination = CGPointMake(bottomOrigin.x - currentBottomRadius, bottomOrigin.y);
-    CGPathAddCurveToPoint(path, NULL, leftCp1.x, leftCp1.y, leftCp2.x, leftCp2.y, leftDestination.x, leftDestination.y);
-    
-    CGPathAddArc(path, NULL, bottomOrigin.x, bottomOrigin.y, currentBottomRadius, M_PI, 0, NO);
-    CGPoint rightCp2 = CGPointMake(lerp((topOrigin.x + currentTopRadius),
+	CGPoint leftDestination = CGPointMake(bottomOrigin.x - currentBottomRadius, bottomOrigin.y);
+	CGPathAddCurveToPoint(path, NULL, leftCp1.x, leftCp1.y, leftCp2.x, leftCp2.y, leftDestination.x, leftDestination.y);
+	
+	CGPathAddArc(path, NULL, bottomOrigin.x, bottomOrigin.y, currentBottomRadius, M_PI, 0, NO);
+	CGPoint rightCp2 = CGPointMake(lerp((topOrigin.x + currentTopRadius),
 										(bottomOrigin.x + currentBottomRadius), 0.1),
 								   lerp(topOrigin.y, bottomOrigin.y, 0.2));
-    CGPoint rightCp1 = CGPointMake(lerp((topOrigin.x + currentTopRadius),
+	CGPoint rightCp1 = CGPointMake(lerp((topOrigin.x + currentTopRadius),
 										(bottomOrigin.x + currentBottomRadius), 0.9),
 								   lerp(topOrigin.y, bottomOrigin.y, 0.2));
-    CGPoint rightDestination = CGPointMake(topOrigin.x + currentTopRadius, topOrigin.y);
-    CGPathAddCurveToPoint(path, NULL, rightCp1.x, rightCp1.y, rightCp2.x, rightCp2.y, rightDestination.x, rightDestination.y);
+	CGPoint rightDestination = CGPointMake(topOrigin.x + currentTopRadius, topOrigin.y);
+	CGPathAddCurveToPoint(path, NULL, rightCp1.x, rightCp1.y, rightCp2.x, rightCp2.y, rightDestination.x, rightDestination.y);
 	CGPathCloseSubpath(path);
 	
 	CGMutablePathRef arrowPath = CGPathCreateMutable();
@@ -222,7 +222,7 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p) {
 	self.shapeLayer.path = path;
 	self.arrowLayer.path = arrowPath;
 	
-    if(refreshTriggered) {
+	if(refreshTriggered) {
 		[TUIView setAnimationsEnabled:NO block:^{
 			self.activity.frame = CGRectMake(self.bounds.size.width / 2 - self.activity.bounds.size.width / 2,
 											 TUIRefreshMaxDistance / 2 - self.activity.bounds.size.height / 2,
@@ -230,55 +230,55 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p) {
 		}];
 		
 		CGMutablePathRef toPath = CGPathCreateMutable();
-        CGPathAddArc(toPath, NULL, topOrigin.x, topOrigin.y, radius, 0, M_PI, YES);
-        CGPathAddCurveToPoint(toPath, NULL, topOrigin.x - radius, topOrigin.y,
+		CGPathAddArc(toPath, NULL, topOrigin.x, topOrigin.y, radius, 0, M_PI, YES);
+		CGPathAddCurveToPoint(toPath, NULL, topOrigin.x - radius, topOrigin.y,
 							  topOrigin.x - radius, topOrigin.y,
 							  topOrigin.x - radius, topOrigin.y);
-        CGPathAddArc(toPath, NULL, topOrigin.x, topOrigin.y, radius, M_PI, 0, YES);
-        CGPathAddCurveToPoint(toPath, NULL, topOrigin.x + radius, topOrigin.y,
+		CGPathAddArc(toPath, NULL, topOrigin.x, topOrigin.y, radius, M_PI, 0, YES);
+		CGPathAddCurveToPoint(toPath, NULL, topOrigin.x + radius, topOrigin.y,
 							  topOrigin.x + radius, topOrigin.y,
 							  topOrigin.x + radius, topOrigin.y);
-        CGPathCloseSubpath(toPath);
+		CGPathCloseSubpath(toPath);
 		
-        CABasicAnimation *pathMorph = [CABasicAnimation animationWithKeyPath:@"path"];
-        pathMorph.duration = 0.15f;
-        pathMorph.fillMode = kCAFillModeForwards;
-        pathMorph.removedOnCompletion = NO;
-        pathMorph.toValue = (__bridge id)toPath;
+		CABasicAnimation *pathMorph = [CABasicAnimation animationWithKeyPath:@"path"];
+		pathMorph.duration = 0.15f;
+		pathMorph.fillMode = kCAFillModeForwards;
+		pathMorph.removedOnCompletion = NO;
+		pathMorph.toValue = (__bridge id)toPath;
 		
-        CABasicAnimation *shapeAlphaAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
-        shapeAlphaAnimation.duration = 0.1f;
-        shapeAlphaAnimation.beginTime = CACurrentMediaTime() + 0.1f;
-        shapeAlphaAnimation.toValue = [NSNumber numberWithFloat:0.0f];
-        shapeAlphaAnimation.fillMode = kCAFillModeForwards;
-        shapeAlphaAnimation.removedOnCompletion = NO;
+		CABasicAnimation *shapeAlphaAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+		shapeAlphaAnimation.duration = 0.1f;
+		shapeAlphaAnimation.beginTime = CACurrentMediaTime() + 0.1f;
+		shapeAlphaAnimation.toValue = [NSNumber numberWithFloat:0.0f];
+		shapeAlphaAnimation.fillMode = kCAFillModeForwards;
+		shapeAlphaAnimation.removedOnCompletion = NO;
 		
-        CABasicAnimation *alphaAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
-        alphaAnimation.duration = 0.1f;
-        alphaAnimation.toValue = [NSNumber numberWithFloat:0.0f];
-        alphaAnimation.fillMode = kCAFillModeForwards;
-        alphaAnimation.removedOnCompletion = NO;
+		CABasicAnimation *alphaAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+		alphaAnimation.duration = 0.1f;
+		alphaAnimation.toValue = [NSNumber numberWithFloat:0.0f];
+		alphaAnimation.fillMode = kCAFillModeForwards;
+		alphaAnimation.removedOnCompletion = NO;
 		
-        [self.shapeLayer addAnimation:pathMorph forKey:nil];
-        [self.shapeLayer addAnimation:shapeAlphaAnimation forKey:nil];
-        [self.arrowLayer addAnimation:alphaAnimation forKey:nil];
+		[self.shapeLayer addAnimation:pathMorph forKey:nil];
+		[self.shapeLayer addAnimation:shapeAlphaAnimation forKey:nil];
+		[self.arrowLayer addAnimation:alphaAnimation forKey:nil];
 		
-        [TUIView animateWithDuration:0.2f animations:^{
-            self.activity.alpha = 1.0f;
+		[TUIView animateWithDuration:0.2f animations:^{
+			self.activity.alpha = 1.0f;
 			[self.activity startAnimating];
 			
 			TUIEdgeInsets preInset = self.tableView.contentInset;
 			preInset.top = -(self.bounds.origin.y - TUIRefreshMaxDistance);
 			self.tableView.contentInset = preInset;
-        }];
-        
-        self.refreshing = YES;
-        [self sendActionsForControlEvents:TUIControlEventValueChanged];
+		}];
+		
+		self.refreshing = YES;
+		[self sendActionsForControlEvents:TUIControlEventValueChanged];
 		
 		CGPathRelease(toPath);
-    }
-    
-    CGPathRelease(path);
+	}
+	
+	CGPathRelease(path);
 	CGPathRelease(arrowPath);
 }
 
